f47c12f3f3ade3ff09170f3ded111a53d3c0dab6
kunhee94 <97655672+kunhee94@users.noreply.github.com>
2022-06-01 22:37:39 +0900

    java 1st

---
+++ selfstudy/Java/1st_java/src/test.java
@@ -0,0 +1,12 @@
+
+public class test {
+	public static void main(String[] args) {
+			System.out.print("Hello world \n");
+			System.out.println("Hello world2");
+			System.out.println("\\");
+			System.out.println("\"");			
+			System.out.printf("%d \n", 10);			
+			System.out.printf("%o \n", 10);			
+			System.out.printf("%x \n", 10);			
+		}
+	}
